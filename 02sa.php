ddbc9c31cd616a4390753a750f70ad9f2d980e22
unknown <202080469@psu.palawan.edu.ph>
2022-09-11 23:20:42 +0800

    02sa.php done

---
+++ 02sa.php
@@ -0,0 +1,21 @@
+<?php
+/*
+After watching the **Functions** write PHP code for this problem.
+
+1. Inside /php-fiesta-cookie create another file named **02sa.php**
+2. Create a function using this template hp_sum, ave_attack
+   - hp_sum will return the sum of the column hp
+   - ave_attack will return the average of column attack
+3.  In CLI, git add, commit and push. [message in commit: "02sa.php done"]
+*/
+$hp = array(45, 60, 80, 39, 58, 78, 44, 59, 79, 50);
+$attack = array(49, 62, 82, 52, 64, 84, 48, 63, 83, 20);
+$zero = 0;
+
+function addnumbers($hp){
+for ($hp($i) = 0;$hp($i) <= 9;$i++) {
+$zero + $hp($i);
+}
+
+
+?>
